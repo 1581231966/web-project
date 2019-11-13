e73afb73d70078a286aaec14bb4adc4e8ee3e694
EHI\Terencel <terence.lin@ehealth.com>
2019-11-13 16:04:17 +0800

    Initial commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Terencel on 2019/11/13.
+ */
+let myHeading = document.querySelector('h2');
+if(myHeading.textContent != 'Record my life')
+{
+    myHeading.textContent = 'Hello';
+}else{
+    alert('This is a alert')
+}
